db11a3571214e684a396b3f2770885be0f4cf1e7
Grant <grant.cates@gmail.com>
2018-10-22 15:58:52 +0200

    Collected pe data on JUSTUS

--- dft_ff/run.sh
+++ dft_ff/run.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-cp calc_pe.in calc_pe_$1.in; sed -i s/'NN'/$1/g calc_pe_$1.in; lmp_icc_serial < calc_pe_$1.in
+for i in {1..20}:
+do ./calc_pe.sh ${i}
+done
+
+grep '^       0' | tee pe.dat
