5f3ed86e1d1282bbc8c8182b911f0ad058391088
Grant Cates <gcates@cip3.icp.uni-stuttgart.de>
2018-10-24 14:11:03 +0200

    Playing around with fit functions

---
+++ dft_ff/data/0.847N_50/water/test2.py
@@ -0,0 +1,34 @@
+import numpy, scipy.optimize
+import matplotlib.pyplot as plt
+
+def fit_sin(tt, yy):
+    '''Fit sin to the inumpyut time sequence, and return fitting parameters "amp", "omega", "phase", "offset", "freq", "period" and "fitfunc"'''
+    tt = numpy.array(tt)
+    yy = numpy.array(yy)
+    ff = numpy.fft.fftfreq(len(tt), (tt[1]-tt[0]))   # assume uniform spacing
+    Fyy = abs(numpy.fft.fft(yy))
+    guess_freq = abs(ff[numpy.argmax(Fyy[1:])+1])   # excluding the zero frequency "peak", which is related to offset
+    guess_amp = numpy.std(yy) * 2.**0.5
+    guess_offset = numpy.mean(yy)
+    guess = numpy.array([guess_amp, 2.*numpy.pi*guess_freq, 0., guess_offset])
+
+    def sinfunc(t, A, w, p, c):  return A * numpy.sin(w*t + p) + c
+    popt, pcov = scipy.optimize.curve_fit(sinfunc, tt, yy, p0=guess)
+    A, w, p, c = popt
+    f = w/(2.*numpy.pi)
+    fitfunc = lambda t: A * numpy.sin(w*t + p) + c
+    return {"amp": A, "omega": w, "phase": p, "offset": c, "freq": f, "period": 1./f, "fitfunc": fitfunc, "maxcov": numpy.max(pcov), "rawres": (guess,popt,pcov)}	
+
+
+
+with open("test.dat","r") as f:
+	data = f.read().splitlines()
+	data = [float(i) for i in data]
+	tt = numpy.linspace(0.2*numpy.pi,50)
+	res = fit_sin(tt, data)
+	print( "Amplitude=%(amp)s, Angular freq.=%(omega)s, phase=%(phase)s, offset=%(offset)s, Max. Cov.=%(maxcov)s" % res )
+
+	plt.plot(tt, data, "ok", label="y with noise")
+	plt.plot(tt, res["fitfunc"](tt), "r-", label="y fit curve", linewidth=2)
+	plt.legend(loc="best")
+	plt.show()
