049bb843e3aeab71350c8cb81f1467ffde1703b0
Grant <grant.cates@gmail.com>
2018-10-25 14:43:20 +0200

    Housekeeping

--- prod/8x8x2/dry/run.sh
+++ prod/8x8x2/dry/run.sh
@@ -4,4 +4,4 @@ cp run.in $1+run.in
 sed -i s/'PROBETEMP'/$1/g $1+run.in
 lmp_icc_serial < $1+run.in
 rm $1+run.in
-mv data/current_run/cp_rawTemp$1.dat data/current_run/rawTemp$1.dat
+mv data/current_run/cp_rawTemp$1.dat data/current_run/equil_rawTemp$1.dat
