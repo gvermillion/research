1bdbc83d997c888b40b7a519d8e0e155f3680bc4
Grant <grant.cates@gmail.com>
2018-11-08 19:48:48 +0100

    Housekeeping

--- prod/8x8x2/dry/run.sh
+++ prod/8x8x2/dry/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-mpirun --bind-to core --map-by core lmp_icc_openmpi < run_$1.in
-rm run_$1.in
-rm run_$1.moab
+mpirun --bind-to core --map-by core lmp_icc_openmpi < tmp/run_$1.in
+rm tmp/run_$1.in
+rm tmp/run_$1.moab
 mv data/current_run/cp_rawTemp$1.dat data/current_run/rawTemp$1.dat
--- prod/8x8x2/dry/scan.sh
+++ prod/8x8x2/dry/scan.sh
@@ -1,5 +1,5 @@
-cp def/run_def.in run_$1.in
-sed -i s/'PROBETEMP'/$1/g run_$1.in	
-cp def/run_def.moab run_$1.moab
-sed -i s/'PROBETEMP'/$1/g run_$1.moab	
+cp def/run_def.in tmp/run_$1.in
+sed -i s/'PROBETEMP'/$1/g tmp/run_$1.in	
+cp def/run_def.moab tmp/run_$1.moab
+sed -i s/'PROBETEMP'/$1/g tmp/run_$1.moab	
 msub run_$1.moab	
