33899281dfdcd483f23dda30621bf5899e74f669
Grant <grant.cates@gmail.com>
2018-11-11 19:34:53 +0100

    Bug fix

--- prod/8x8x2/dry/scan_temp.sh
+++ prod/8x8x2/dry/scan_temp.sh
@@ -1,8 +1,8 @@
 for i in {12..100}
 	do
-		cp def/run_def.in run_${i}.in
-		sed -i s/'PROBETEMP'/${i}/g run_${i}.in	
-		cp def/run_def.moab run_${i}.moab
-		sed -i s/'PROBETEMP'/${i}/g run_${i}.moab	
-		msub run_${i}.moab	
+		cp def/run_def.in tmp/run_${i}.in
+		sed -i s/'PROBETEMP'/${i}/g tmp/run_${i}.in	
+		cp def/run_def.moab tmp/run_${i}.moab
+		sed -i s/'PROBETEMP'/${i}/g tmp/run_${i}.moab	
+		msub tmp/run_${i}.moab	
 	done
